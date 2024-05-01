3802f31210ce824bd1d81252f0784566359ceb3f
Bastien Orivel <eijebong@bananium.fr>
2024-05-01 14:00:12 +0200

    Also return a list of unsupported games and skip checking those
    This will allow the lobby to display a warning (or an error, based on
    room config) about those

--- check.py
+++ check.py
@@ -1,5 +1,6 @@
 from Generate import roll_settings
 from Utils import parse_yamls
+from worlds.AutoWorld import AutoWorldRegister
 from flask import Flask, request
 
 import copy
@@ -10,17 +11,31 @@ app = Flask(__name__)
 def check_yaml():
     yaml_content = request.form["data"]
     parsed_yamls = parse_yamls(yaml_content)
+    unsupported = []
+
     for yaml in parsed_yamls:
         if 'game' not in yaml:
             return {"error": "This doesn't look like an archipelago YAML? Missing game"}
 
         game = yaml['game']
         if isinstance(game, str):
-            result, err = check_yaml(yaml)
+            if is_supported(game):
+                result, err = check_yaml(yaml)
+            else:
+                result = True
+                err = "Unsupported"
+                unsupported = game
         else:
             for game, weight in game.items():
                 if weight == 0:
                     continue
+
+                if not is_supported(game):
+                    result = True
+                    err = "Unsupported"
+                    unsupported.append(game)
+                    continue
+
                 yaml_for_game = copy.deepcopy(yaml);
                 for yaml_game in yaml_for_game['game']:
                     yaml_for_game['game'][yaml_game] = 1 if yaml_game == game else 0
@@ -30,9 +45,9 @@ def check_yaml():
                     break
 
     if result:
-        return {}
+        return {"unsupported": unsupported}
 
-    return {"error": err}
+    return {"error": err, "unsupported": unsupported}
 
 
 def check_yaml(yaml):
@@ -46,5 +61,8 @@ def check_yaml(yaml):
             return False, f"Validation error: {e}"
 
     return True, "OK"
+
+def is_supported(game):
+    return game in AutoWorldRegister.world_types
 
 app.run(host="0.0.0.0")
