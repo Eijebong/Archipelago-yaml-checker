5e5c123b56f21fd2e68893caa308ab1f72e4a2e4
Bastien Orivel <eijebong@bananium.fr>
2024-05-01 13:25:54 +0200

    Validate every game in a multi game YAML
    roll_options was rolling the game and then validating that game's
    options only. Which means you could get game A validated on upload and
    then get game B rolled on generation time which could've contained
    invalid options.
    
    This fixes it by validating the YAML for every game, changing all
    weights to 0 except for the game it's validating, forcing roll_settings
    to roll that one game.

--- check.py
+++ check.py
@@ -1,17 +1,50 @@
-from WebHostLib.check import roll_options
+from Generate import roll_settings
+from Utils import parse_yamls
 from flask import Flask, request
+
+import copy
 
 app = Flask(__name__)
 
 @app.route("/check_yaml", methods=["POST"])
-def hello_world():
+def check_yaml():
     yaml_content = request.form["data"]
-    results, _ = roll_options({"file.yaml": yaml_content})
+    parsed_yamls = parse_yamls(yaml_content)
+    for yaml in parsed_yamls:
+        if 'game' not in yaml:
+            return {"error": "This doesn't look like an archipelago YAML? Missing game"}
 
-    file_result = results["file.yaml"]
-    if file_result is True:
+        game = yaml['game']
+        if isinstance(game, str):
+            result, err = check_yaml(yaml)
+        else:
+            for game, weight in game.items():
+                if weight == 0:
+                    continue
+                yaml_for_game = copy.deepcopy(yaml);
+                for yaml_game in yaml_for_game['game']:
+                    yaml_for_game['game'][yaml_game] = 1 if yaml_game == game else 0
+
+                result, err = check_yaml(yaml_for_game)
+                if not result:
+                    break
+
+    if result:
         return {}
 
-    return {"error": file_result}
+    return {"error": err}
+
+
+def check_yaml(yaml):
+    plando_options = frozenset({"bosses", "items", "connections", "texts"})
+    try:
+        roll_settings(yaml)
+    except Exception as e:
+        if e.__cause__:
+            return False, f"Validation error: {e} - {e.__cause__}"
+        else:
+            return False, f"Validation error: {e}"
+
+    return True, "OK"
 
 app.run(host="0.0.0.0")
