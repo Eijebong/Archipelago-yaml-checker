a28a8a182c6a2e8b59aa7a67237f4235d0b47f88
Bastien Orivel <eijebong@bananium.fr>
2024-07-25 12:29:16 +0200

    Apparently we can't use custom_worlds outside of non source install

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ cd /ap/archipelago
 . .venv/bin/activate
 
 while true; do
-    apwm install -i /index -a /apworlds -d /ap/archipelago/custom_worlds
+    apwm install -i /index -a /apworlds -d /ap/archipelago/worlds
     python3 check.py
 done
