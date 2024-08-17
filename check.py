ebcc6dfa763a1c31c91c82cad79e1dfc7824d280
Bastien Orivel <eijebong@bananium.fr>
2024-08-17 19:18:18 +0200

    Add OTLP support

--- check.py
+++ check.py
@@ -1,3 +1,10 @@
+from opentelemetry import trace
+from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
 from Generate import roll_settings, PlandoOptions
 from Utils import parse_yamls
 from worlds.AutoWorld import AutoWorldRegister, call_all
@@ -9,6 +16,21 @@ import os
 
 import copy
 
+
+tracer = trace.get_tracer("yaml-validator")
+resource = Resource(attributes={
+    SERVICE_NAME: "yaml-checker"
+})
+
+otlp_endpoint = os.environ.get("OTLP_ENDPOINT")
+if otlp_endpoint:
+    traceProvider = TracerProvider(resource=resource)
+    processor = BatchSpanProcessor(OTLPSpanExporter(endpoint=otlp_endpoint))
+    traceProvider.add_span_processor(processor)
+    trace.set_tracer_provider(traceProvider)
+else:
+    print("OTLP_ENDPOINT not provided, not enabling otlp exporter")
+
 app = Flask(__name__)
 
 @app.route("/restart", methods=["GET"])
@@ -17,48 +39,53 @@ def restart():
 
 @app.route("/check_yaml", methods=["POST"])
 def check_yaml():
-    yaml_content = request.form["data"]
-    parsed_yamls = parse_yamls(yaml_content)
-    unsupported = []
+    ctx = TraceContextTextMapPropagator().extract(carrier=request.headers)
+    with tracer.start_as_current_span("check_yamls", context=ctx) as span:
+        yaml_content = request.form["data"]
+        parsed_yamls = parse_yamls(yaml_content)
+        unsupported = []
 
-    for yaml in parsed_yamls:
-        if 'game' not in yaml:
-            return {"error": "This doesn't look like an archipelago YAML? Missing game"}
+        for yaml in parsed_yamls:
+            if 'game' not in yaml:
+                return {"error": "This doesn't look like an archipelago YAML? Missing game"}
 
-        game = yaml['game']
-        if isinstance(game, str):
-            if is_supported(game):
-                result, err = check_yaml(game, yaml)
-            else:
-                result = True
-                err = "Unsupported"
-                unsupported = [game,]
-        else:
-            for game, weight in game.items():
-                if weight == 0:
-                    continue
-
-                if not is_supported(game):
+            game = yaml['game']
+            if isinstance(game, str):
+                if is_supported(game):
+                    result, err = check_yaml(game, yaml)
+                else:
                     result = True
                     err = "Unsupported"
-                    unsupported.append(game)
-                    continue
+                    unsupported = [game,]
+            else:
+                for game, weight in game.items():
+                    if weight == 0:
+                        continue
 
-                yaml_for_game = copy.deepcopy(yaml);
-                for yaml_game in yaml_for_game['game']:
-                    yaml_for_game['game'][yaml_game] = 1 if yaml_game == game else 0
+                    if not is_supported(game):
+                        result = True
+                        err = "Unsupported"
+                        unsupported.append(game)
+                        continue
 
-                result, err = check_yaml(game, yaml_for_game)
-                if not result:
-                    break
+                    yaml_for_game = copy.deepcopy(yaml);
+                    for yaml_game in yaml_for_game['game']:
+                        yaml_for_game['game'][yaml_game] = 1 if yaml_game == game else 0
 
-    if result:
-        return {"unsupported": unsupported}
+                    result, err = check_yaml(game, yaml_for_game)
+                    if not result:
+                        break
 
-    return {"error": err, "unsupported": unsupported}
+        if result:
+            return {"unsupported": unsupported}
+
+        return {"error": err, "unsupported": unsupported}
 
 
+@tracer.start_as_current_span("check_yaml")
 def check_yaml(game, yaml):
+    span = trace.get_current_span()
+    span.set_attribute("game", game)
     plando_options = PlandoOptions.from_set(frozenset({"bosses", "items", "connections", "texts"}))
     try:
         world_type = AutoWorldRegister.world_types[game]
@@ -68,12 +95,11 @@ def check_yaml(game, yaml):
         multiworld.set_seed(0)
         multiworld.state = CollectionState(multiworld)
 
+        span.add_event("Rolling settings")
         yaml_args = vars(roll_settings(yaml, plando_options))
+        span.add_event("Settings rolled")
 
-        # Skip generate_early for Zillion as it generates the level layout which is way too slow
-        if game in ["Zillion"]:
-            return True, "OK"
-
+        span.add_event("Verifying keys")
         args = Namespace()
         for name, option in world_type.options_dataclass.type_hints.items():
             value = yaml_args.get(name, option.default)
@@ -82,9 +108,15 @@ def check_yaml(game, yaml):
                 option.verify_keys(value.value)
 
             setattr(args, name, {1: value})
+        span.add_event("Keys verified")
 
-        multiworld.set_options(args)
-        call_all(multiworld, "generate_early")
+        # Skip generate_early for Zillion as it generates the level layout which is way too slow
+        if game in ["Zillion"]:
+            return True, "OK"
+
+        with tracer.start_span("generate_early"):
+            multiworld.set_options(args)
+            call_all(multiworld, "generate_early")
     except Exception as e:
         if e.__cause__:
             return False, f"Validation error: {e} - {e.__cause__}"
