f200d44000ef52fb3b908632e5f774d34d8a2ecd
Bastien Orivel <eijebong@bananium.fr>
2024-08-17 20:52:51 +0200

    Remove loop now that we don't need to restart ourselves

--- run.sh
+++ run.sh
@@ -3,8 +3,6 @@
 cd /ap/archipelago
 . .venv/bin/activate
 
-while true; do
-    rm -Rf /ap/archipelago/custom_worlds/*
-    apwm install -i /index -a /apworlds -d /ap/archipelago/custom_worlds
-    python3 check.py
-done
+rm -Rf /ap/archipelago/custom_worlds/*
+apwm install -i /index -a /apworlds -d /ap/archipelago/custom_worlds
+python3 check.py
