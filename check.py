d7ab1029cbe9f236c2fb6295ec08a9585dc68a5d
Bastien Orivel <eijebong@bananium.fr>
2024-09-10 18:38:27 +0200

    Run a lot more early generation to catch more YAML errors
    This calls things up to `generate_basic` now with the block following
    checking for item removal from the pool
    
    While this makes YAML validation slower, I checked with all current
    apworlds in my index, the slowdown was minor except for
    monster_sanctuary which had a very slow `create_items` phase and was an
    easy fix: https://github.com/Gtaray/Archipelago/pull/1
    
    Fixes #1

--- check.py
+++ check.py
@@ -8,9 +8,10 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from Generate import roll_settings, PlandoOptions
 from Utils import parse_yamls
 from worlds.AutoWorld import AutoWorldRegister, call_all, World
+from worlds.generic.Rules import exclusion_rules, locality_rules
 from argparse import Namespace
-from Options import VerifyKeys
-from BaseClasses import CollectionState, MultiWorld
+from Options import VerifyKeys, PerGameCommonOptions, StartInventoryPool
+from BaseClasses import CollectionState, MultiWorld, LocationProgressType
 from worlds import WorldSource
 
 import copy
@@ -99,6 +100,7 @@ def check_request(ctx, apworlds, data, wpipe):
             result = load_apworlds_and_check(ctx, apworlds, data)
             wpipe.send(result)
         except Exception as e:
+            span.record_exception(e)
             wpipe.send({"error": f"{e}"})
 
     if otlp_endpoint:
@@ -155,6 +157,7 @@ class DummyWorld(World):
     game = "Dummy World"
     item_name_to_id = {}
     location_name_to_id = {}
+    options_dataclass = PerGameCommonOptions
 
 @tracer.start_as_current_span("check_yaml")
 def check_yaml(game, name, yaml):
@@ -170,25 +173,129 @@ def check_yaml(game, name, yaml):
         multiworld.state = CollectionState(multiworld)
 
         span.add_event("Rolling settings")
-        yaml_args = vars(roll_settings(yaml, plando_options))
+        erargs = Namespace()
+        settings = roll_settings(yaml, plando_options)
         span.add_event("Settings rolled")
 
-        span.add_event("Verifying keys")
-        args = Namespace()
-        for name, option in world_type.options_dataclass.type_hints.items():
-            value = yaml_args.get(name, option.default)
+        for k, v in vars(settings).items():
+            if v is not None:
+                try:
+                    getattr(erargs, k)[1] = v
+                except AttributeError:
+                    setattr(erargs, k, {1: v})
+                except Exception as e:
+                    raise Exception(f"Error setting {k} to {v} for player") from e
 
-            setattr(args, name, {1: value, 2: {}})
-        span.add_event("Keys verified")
+        for option_name, option in DummyWorld.options_dataclass.type_hints.items():
+            getattr(erargs, option_name)[2] = option(option.default)
 
         # Skip generate_early for Zillion as it generates the level layout which is way too slow
         if game in ["Zillion"]:
             return True, "OK"
 
+        multiworld.set_options(erargs)
+        multiworld.set_item_links()
+
         with tracer.start_span("generate_early"):
-            multiworld.set_options(args)
             call_all(multiworld, "generate_early")
+
+        # this whole block is basically https://github.com/ArchipelagoMW/Archipelago/blob/7ff201e32c859eeb1b3e07ee087f11da3249f833/Generate.py#L68
+        # except it's adapted to generation with 2 YAMLs only, with one being a dummy one using `DummyWorld` above
+
+        for player in multiworld.player_ids:
+            for item_name, count in multiworld.worlds[player].options.start_inventory.value.items():
+                for _ in range(count):
+                    multiworld.push_precollected(multiworld.create_item(item_name, player))
+
+            for item_name, count in getattr(multiworld.worlds[player].options,
+                                            "start_inventory_from_pool",
+                                            StartInventoryPool({})).value.items():
+                for _ in range(count):
+                    multiworld.push_precollected(multiworld.create_item(item_name, player))
+                # remove from_pool items also from early items handling, as starting is plenty early.
+                early = multiworld.early_items[player].get(item_name, 0)
+                if early:
+                    multiworld.early_items[player][item_name] = max(0, early-count)
+                    remaining_count = count-early
+                    if remaining_count > 0:
+                        local_early = multiworld.early_local_items[player].get(item_name, 0)
+                        if local_early:
+                            multiworld.early_items[player][item_name] = max(0, local_early - remaining_count)
+                        del local_early
+                del early
+
+        with tracer.start_span("generate_regions"):
+            call_all(multiworld, "create_regions")
+
+        with tracer.start_span("create_items"):
+            call_all(multiworld, "create_items")
+
+        for player in multiworld.player_ids:
+            # items can't be both local and non-local, prefer local
+            multiworld.worlds[player].options.non_local_items.value -= multiworld.worlds[player].options.local_items.value
+            multiworld.worlds[player].options.non_local_items.value -= set(multiworld.local_early_items[player])
+
+        with tracer.start_span("set_rules"):
+            call_all(multiworld, "set_rules")
+
+        for player in multiworld.player_ids:
+            exclusion_rules(multiworld, player, multiworld.worlds[player].options.exclude_locations.value)
+            multiworld.worlds[player].options.priority_locations.value -= multiworld.worlds[player].options.exclude_locations.value
+            for location_name in multiworld.worlds[player].options.priority_locations.value:
+                try:
+                    location = multiworld.get_location(location_name, player)
+                except KeyError as e:  # failed to find the given location. Check if it's a legitimate location
+                    if location_name not in multiworld.worlds[player].location_name_to_id:
+                        raise Exception(f"Unable to prioritize location {location_name} in player {player}'s world.") from e
+                else:
+                    location.progress_type = LocationProgressType.PRIORITY
+
+        locality_rules(multiworld)
+
+        with tracer.start_span("generate_basic"):
+            call_all(multiworld, "generate_basic")
+
+        # remove starting inventory from pool items.
+        # Because some worlds don't actually create items during create_items this has to be as late as possible.
+        if any(getattr(multiworld.worlds[player].options, "start_inventory_from_pool", None) for player in multiworld.player_ids):
+            new_items: List[Item] = []
+            depletion_pool: Dict[int, Dict[str, int]] = {
+                player: getattr(multiworld.worlds[player].options,
+                                "start_inventory_from_pool",
+                                StartInventoryPool({})).value.copy()
+                for player in multiworld.player_ids
+            }
+            for player, items in depletion_pool.items():
+                player_world = multiworld.worlds[player]
+                for count in items.values():
+                    for _ in range(count):
+                        new_items.append(player_world.create_filler())
+            target: int = sum(sum(items.values()) for items in depletion_pool.values())
+            for i, item in enumerate(multiworld.itempool):
+                if depletion_pool[item.player].get(item.name, 0):
+                    target -= 1
+                    depletion_pool[item.player][item.name] -= 1
+                    # quick abort if we have found all items
+                    if not target:
+                        new_items.extend(multiworld.itempool[i+1:])
+                        break
+                else:
+                    new_items.append(item)
+
+            # leftovers?
+            if target:
+                for player, remaining_items in depletion_pool.items():
+                    remaining_items = {name: count for name, count in remaining_items.items() if count}
+                    if remaining_items:
+                        raise Exception(f"{multiworld.get_player_name(player)}"
+                                        f" is trying to remove items from their pool that don't exist: {remaining_items}")
+            assert len(multiworld.itempool) == len(new_items), "Item Pool amounts should not change."
+            multiworld.itempool[:] = new_items
+
     except Exception as e:
+        span = trace.get_current_span()
+        span.record_exception(e)
+
         if e.__cause__:
             return False, f"Validation error for {name}: {e} - {e.__cause__}"
         else:
