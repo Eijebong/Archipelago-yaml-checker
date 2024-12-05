f6b436b1835424704ef270fe0cb013906b2773c1
Bastien Orivel <eijebong@bananium.fr>
2024-12-05 22:27:02 +0100

    Don't print a weird "None isn't iterable" when submitted a YAML with no settings

--- check.py
+++ check.py
@@ -201,6 +201,8 @@ def check_yaml(game, name, yaml):
 
         span.add_event("Rolling settings")
         erargs = Namespace()
+        if yaml.get(name) is None:
+            raise Exception("Did you submit a game with no settings?")
         settings = roll_settings(yaml, plando_options)
         span.add_event("Settings rolled")
 
