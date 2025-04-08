b2ae3b98ca99712ccf43b73c157598b811f15646
Bastien Orivel <eijebong@bananium.fr>
2025-04-08 19:33:09 +0200

    Prep for 0.6.0 and add classifiers to the image

--- taskcluster/docker/ap-yaml-checker/setup.sh
+++ taskcluster/docker/ap-yaml-checker/setup.sh
@@ -25,14 +25,15 @@ uv pip install -r WebHostLib/requirements.txt
 uv pip install -r worlds/_sc2common/requirements.txt
 uv pip install -r worlds/alttp/requirements.txt
 uv pip install -r worlds/factorio/requirements.txt
-uv pip install -r worlds/hk/requirements.txt
 uv pip install -r worlds/kh2/requirements.txt
 uv pip install -r worlds/minecraft/requirements.txt
 uv pip install -r worlds/sc2/requirements.txt
 uv pip install -r worlds/soe/requirements.txt
 uv pip install -r worlds/tloz/requirements.txt
+uv pip install -r worlds/tww/requirements.txt
 uv pip install -r worlds/zillion/requirements.txt
-uv pip install python-sat==1.8.dev13 opentelemetry-api==1.26.0 opentelemetry-sdk==1.26.0 opentelemetry-exporter-otlp-proto-grpc==1.26.0 aiohttp==3.9.5 "sentry-sdk[opentelemetry]==2.19.2" dolphin-memory-engine==1.3.0
+uv pip install -r worlds/zork_grand_inquisitor/requirements.txt
+uv pip install python-sat==1.8.dev13 opentelemetry-api==1.26.0 opentelemetry-sdk==1.26.0 opentelemetry-exporter-otlp-proto-grpc==1.26.0 aiohttp==3.9.5 "sentry-sdk[opentelemetry]==2.19.2"
 git rev-parse HEAD > /ap/archipelago/version
 mkdir -p /ap/supported_worlds
 chown -R worker:worker /ap/archipelago
@@ -41,7 +42,18 @@ echo -e "jakanddaxter_options:\n  enforce_friendly_options: false" > /ap/archipe
 bash /ap/archipelago/prepare_worlds.sh /ap/archipelago /ap/supported_worlds
 rm /ap/archipelago/prepare_worlds.sh
 
-curl https://raw.githubusercontent.com/Eijebong/Archipelago-fuzzer/${FUZZER_COMMIT}/fuzz.py -o /ap/archipelago/fuzz.py
+mkdir /tmp/fuzzer
+pushd /tmp/fuzzer
+git init
+git remote add origin https://github.com/Eijebong/Archipelago-fuzzer.git
+git fetch origin ${FUZZER_COMMIT} --depth 1
+git reset --hard ${FUZZER_COMMIT}
+cp fuzz.py /ap/archipelago/fuzz.py
+cp -R classifiers /ap/archipelago/classifiers/
+touch /ap/archipelago/classifiers/__init__.py
+popd
+rm -Rf /tmp/fuzzer
+
 chmod +x /ap/enemizer/EnemizerCLI.Core
 
 apt purge -y gcc python3-dev
